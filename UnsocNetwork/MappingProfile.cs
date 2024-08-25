6597e2deac4d36ea8113cddef8afb9e5dac1f436
Shatalov.AG <mrstep93@ya.ru>
2024-08-25 15:26:45 +0300

    mapping datetime

--- UnsocNetwork/MappingProfile.cs
+++ UnsocNetwork/MappingProfile.cs
@@ -1,11 +1,23 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Net;
 using UnsocNetwork.Models;
 using UnsocNetwork.ViewModels.Account;
 
 namespace UnsocNetwork
 {
+
+    public class Source
+    {
+        public int Value { get; set; }
+    }
+
+    public class Destination
+    {
+        public string Value { get; set; }
+    }
+
     public class MappingProfile : Profile
     {
         /// <summary>
@@ -13,8 +25,17 @@ namespace UnsocNetwork
         /// </summary>
         public MappingProfile()
         {
-            CreateMap<RegisterViewModel,User>();
+            CreateMap<RegisterViewModel, User>()
+                .ForMember("Email", dest => dest.MapFrom(src => src.EmailReg))
+                .ForMember("NickName", dest => dest.MapFrom(src => src.FirstName))
+                .ForMember("BirthDate", opt => opt.ConvertUsing(src => new DateTime(src.Year, src.Month, src.Date)));
 
+            CreateMap<Source, Destination>()
+        .ForMember(dest => dest.Value, opt => opt.ConvertUsing(src => src.Value.ToString()));
+
+            //.ForMember("BirthDate", dest => dest.MapFrom(src => src.)
+
+            // .ForMember("Name", opt => opt.MapFrom(c => c.FirstName + " " + c.LastName))
         }
     }
 }
