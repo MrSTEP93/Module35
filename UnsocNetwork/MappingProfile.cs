6a9fbd490985ce6289f2987d178b29a91cdd08a0
Shatalov.AG <mrstep93@ya.ru>
2024-08-25 15:39:52 +0300

    нифига

--- UnsocNetwork/MappingProfile.cs
+++ UnsocNetwork/MappingProfile.cs
@@ -30,12 +30,33 @@ namespace UnsocNetwork
                 .ForMember("NickName", dest => dest.MapFrom(src => src.FirstName))
                 .ForMember("BirthDate", opt => opt.ConvertUsing(src => new DateTime(src.Year, src.Month, src.Date)));
 
+
+            // простые образцы из интернета тоже не работают =(
             CreateMap<Source, Destination>()
-        .ForMember(dest => dest.Value, opt => opt.ConvertUsing(src => src.Value.ToString()));
+                .ForMember(dest => dest.Value, opt => opt.ConvertUsing(src => src.Value.ToString()));
+
+            // даже со своим конвертером
+            CreateMap<string, DateTime>()
+                .ConvertUsing(new DateTimeTypeConverter());
 
             //.ForMember("BirthDate", dest => dest.MapFrom(src => src.)
 
             // .ForMember("Name", opt => opt.MapFrom(c => c.FirstName + " " + c.LastName))
         }
     }
+
+    public class DateTimeTypeConverter : ITypeConverter<int[], DateTime>
+    {
+        public DateTime Convert(string source, DateTime destination, ResolutionContext context)
+        {
+            return System.Convert.ToDateTime(source);
+        }
+
+        public DateTime Convert(int[] source, DateTime destination, ResolutionContext context)
+        {
+            string stringDate = $"{source[0]}.{source[1]}.{source[2]}";
+            return Convert(stringDate, destination, context);
+        }
+    }
+
 }
