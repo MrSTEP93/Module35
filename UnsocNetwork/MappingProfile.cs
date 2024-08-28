3c78871973f000658ae079619106a9b88a61b5da
Shatalov.AG <mrstep93@ya.ru>
2024-08-29 00:21:09 +0300

    checking the models

--- UnsocNetwork/MappingProfile.cs
+++ UnsocNetwork/MappingProfile.cs
@@ -16,7 +16,8 @@ namespace UnsocNetwork
         {
             CreateMap<RegisterViewModel, User>()
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.EmailReg))
-                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.FirstName))
+                //.ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.FirstName))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.EmailReg))
                 .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => new DateTime(src.Year, src.Month, src.Date)));
         }
     }
