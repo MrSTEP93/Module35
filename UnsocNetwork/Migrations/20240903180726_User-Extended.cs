7a246d5f72b955438ce7c48b93b71d5427185466
Shatalov.AG <mrstep93@ya.ru>
2024-09-03 22:18:03 +0300

    success

---
+++ UnsocNetwork/Migrations/20240903180726_User-Extended.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UnsocNetwork.Migrations
+{
+    public partial class UserExtended : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PathToPhoto",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PathToPhoto",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RegDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "SecondName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AspNetUsers");
+        }
+    }
+}
--- UnsocNetwork/Models/User.cs
+++ UnsocNetwork/Models/User.cs
@@ -3,14 +3,30 @@ using System;
 
 namespace UnsocNetwork.Models
 {
-    public class User : IdentityUser
+public class User : IdentityUser
     {
         public string FirstName { get; set; }
 
+        public string SecondName { get; set; }
+
         public string LastName { get; set; }
 
-        //public string Login { get; set; }
-
         public DateTime BirthDate { get; set; }
+
+        public string PathToPhoto { get; set; }
+
+        public string Status { get; set; }
+
+        public string About { get; set; }
+
+        public DateTime RegDate { get; set; }
+
+        public User()
+        {
+            PathToPhoto = "/static/img/NoPhoto.jpg";
+            Status = "Дратути! Я с вами, с усами!";
+            About = "Пришёл, покушал и поспал";
+            RegDate = DateTime.Now;
+        }
     }
 }
